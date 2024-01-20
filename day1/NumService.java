7a66b2d6502abd97846ef5b5ca9500867e36a2f3
aksh-200 <81736871+aksh-200@users.noreply.github.com>
2024-01-20 07:54:40 +0530

    Delete day1 directory

--- day1/NumService.java
+++
@@ -1,6 +0,0 @@
-class NumService{
-	public static float addition(int no1,float no2)
-	{
-		return no1+no2;
-	}
-}
--- day1/NumberService.java
+++
@@ -1,7 +0,0 @@
-class NumberService{
-
-   public static int addition(int num1,int num2)
-   {
-	   return num1 + num2;
-   }   
-}
